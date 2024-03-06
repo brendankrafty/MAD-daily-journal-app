043ef5b31f38bcc9fe68369a10154beb9f52ad8f
RajpreetBains <bainsraj14@gmail.com>
2024-03-06 10:46:06 -0500

    Splash Screen Page

---
+++ daily_journal_app/lib/splash.dart
@@ -0,0 +1,60 @@
+import 'package:daily_journal_app/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+
+class _SplashState extends State<Splash> with SingleTickerProviderStateMixin{
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+    Future.delayed(Duration(seconds: 2), (){
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) => const HomeScreen()));
+    });
+  }
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,overlays: SystemUiOverlay.values);
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              colors: [Colors.white, Colors.white],
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+          )
+        ),
+        child: const Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 20),
+            Text(
+              'Dayz',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.normal,
+                color: Colors.black,
+                fontSize: 54,
+              )
+            )
+          ],
+        ),
+
+      ),
+    );
+  }
+}
