fbf4616feefec57158f3a3be3c1ae6fd42e2ef5f
RajpreetBains <bainsraj14@gmail.com>
2024-03-11 17:51:22 -0400

    Added search bar for homescreen

--- daily_journal_app/lib/home_screen.dart
+++ daily_journal_app/lib/home_screen.dart
@@ -1,17 +1,77 @@
 import 'package:flutter/material.dart';
-class HomeScreen extends StatelessWidget {
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Dayz'),
+      backgroundColor: Colors.grey.shade900,
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(16, 40, 16, 0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'Dayz',
+                  style: TextStyle(fontSize: 30, color: Colors.white),
+                ),
+                IconButton(
+                    onPressed: () {},
+                    padding: const EdgeInsets.all(0),
+                    icon: Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                          color: Colors.grey.shade800.withOpacity(.8),
+                          borderRadius: BorderRadius.circular(10)),
+                      child: const Icon(
+                        Icons.sort,
+                        color: Colors.white,
+                      ),
+                    ))
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              style: const TextStyle(fontSize: 16, color: Colors.white),
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.symmetric(vertical: 12),
+                hintText: "Search notes...",
+                hintStyle: const TextStyle(color: Colors.grey),
+                prefixIcon: const Icon(
+                  Icons.search,
+                  color: Colors.grey,
+                ),
+                fillColor: Colors.grey.shade800,
+                filled: true,
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  borderSide: const BorderSide(color: Colors.transparent),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  borderSide: const BorderSide(color: Colors.transparent),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
-      body: const Center(
-        child: Text('this is'),
-      ),
-
     );
   }
 }
